d9538a2d7bb40c11a061b7113ac5757ca4353fc7
Praharshinibujji <42573056+Praharshinibujji@users.noreply.github.com>
2018-12-08 11:52:57 +0530

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,10 @@
+num14=raw_input()
+num15=raw_input()
+num16=raw_input()
+if (num14>num15) and (num14>num16):
+	largest=num14
+elif (num15>num16) and (num15>num16):
+	largest=num15
+else:
+	largest=num16
+print (largest)
