aa792d58d297fa30a55b8ad73472831b904c368d
Praharshinibujji <42573056+Praharshinibujji@users.noreply.github.com>
2018-12-08 11:31:27 +0530

    Update and rename 7.py to 8.py

--- 7.py
+++
@@ -1,5 +0,0 @@
-str=raw_input()
-if str in ('a','e','i','o','u'):
-	print 'vowel'
-else:
-	print 'consonent'
---
+++ 8.py
@@ -0,0 +1,5 @@
+string=raw_input()
+if string in ('a','e','i','o','u'):
+	print 'vowel'
+else:
+	print 'consonent'
