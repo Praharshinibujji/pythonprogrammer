97f167ad82cfd11e456389c8e39eecc7e7be870d
Praharshinibujji <42573056+Praharshinibujji@users.noreply.github.com>
2018-12-09 18:15:15 +0530

    Rename 34.py to 55.py

---
+++ 55.py
@@ -0,0 +1,8 @@
+k,h=map(int,raw_input().split())
+for n in range(k,h+1):
+    if n>1:
+         for i in range(2,n):
+             if(n%i)==0:
+	      break
+         else:
+               print(n),
